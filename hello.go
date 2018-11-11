1ef0f66d99f4c1826d857e319ad38a6b41f7e67d
Matthias Oneisz <moneisz@gnubis.de>
2018-11-11 12:16:26 +0100

    now with using of stringutil

--- hello.go
+++ hello.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/devtonemad/stringutil"
+)
 
 func main() {
 
 	fmt.Printf("Hello World!\n")
+
+	fmt.Printf(stringutil.Reverse("This is a reversed Message! Can you read?"))
+
+	fmt.Printf("\n")
 
 }
